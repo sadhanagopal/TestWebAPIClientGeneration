5660858bf0738b3bf497d7052ac98ba8ffc53a92
Sadhana Gopal <sgopal@independer.nl>
2018-04-05 15:04:33 +0200

    Add project files.

---
+++ TestWebAPIClientGeneration/Contracts/TestObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebAPIClientGeneration.Contracts
+{
+    public class TestObject
+    {
+      public int ProductId { get; set; }
+    }
+}
---
+++ TestWebAPIClientGeneration/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestWebAPIClientGeneration.Contracts;
+
+namespace TestWebAPIClientGeneration.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public string Get([FromRoute]TestObject testObject)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
